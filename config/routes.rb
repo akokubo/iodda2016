be61a8c70e6fcf1fc2f7aa3031a593c0d5a508b3
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-12-07 20:48:18 +0900

    Bootstrap 3.3.6をレイアウトに導入、名前付きルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+  get 'help' => 'static_pages#help'
 
   resources :municipalities
   # The priority is based upon order of creation: first created -> highest priority.
