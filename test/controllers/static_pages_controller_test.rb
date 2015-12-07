9a6ce87889ded41f2304040a1daec71cecf5624b
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-12-07 13:56:07 +0900

    ページのタイトルとstatic_pages#aboutの実装

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "IODDA2016アプリ"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "ヘルプ | #{@base_title}"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "アプリについて | #{@base_title}"
+  end
 end
