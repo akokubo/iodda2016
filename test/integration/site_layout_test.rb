5de4877190aa84126b7f83b5bacd7bd8bfdfef63
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-12-07 21:15:52 +0900

    市町村モデルの日本語化

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,6 +6,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", municipalities_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
   end
