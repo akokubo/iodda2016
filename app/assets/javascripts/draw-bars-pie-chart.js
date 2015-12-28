7d42e3a8df66fe68db5e582f4e92f8997252f9ab
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-12-28 22:21:26 +0900

    棒グラフと円グラフの表示に関するコードのリファクタリング

--- app/assets/javascripts/draw-bars-pie-chart.js
+++ app/assets/javascripts/draw-bars-pie-chart.js
@@ -1,99 +1,114 @@
 /*jslint browser:true, devel:true */
 /*global $, Flotr */
 
-$(document).ready(
-    function () {
-        'use strict';
 
-        var horizontalData = [];
-        var horizontalXaxisLabels = [];
-        var pieData = [];
+// 棒グラフの表示
+function drawBars(dataset) {
+    'use strict';
 
-        // データの整形
-        function formatData(dataset, horizontalData, horizontalXaxisLabels, pieData) {
-            dataset.data.forEach(function (datum, idx) {
-                horizontalData.push([datum.value, idx]);
-                pieData.push({label: datum.municipality, data: [[0, datum.value]]});
-                horizontalXaxisLabels.push([idx, datum.municipality]);
-            });
+    var barsData,
+        barData = [],
+        yAxisLabels = [],
+        length;
+
+    // データの整形
+    length = dataset.data.length;
+    dataset.data.forEach(function (datum, idx) {
+        // ふつうにデータを作ると、下から上に積みあがるので、それを上下逆に
+        barData.push([datum.value, length - idx - 1]);
+        yAxisLabels.push([length - idx - 1, datum.municipality]);
+    });
+    barsData = [barData];
+
+    // グラフの描画
+    Flotr.draw(
+        $("#horizontal-bars")[0],
+        barsData,
+        {
+            title: dataset.dataset,
+            bars: {
+                show: true,
+                horizontal: true,
+                barWidth: 0.6,
+                shadowSize: 0,
+                fillOpacity: 1,
+                lineWidth: 0
+            },
+            xaxis: {
+                min: 0,
+                tickDecimals: 0
+            },
+            yaxis: {
+                ticks: yAxisLabels
+            },
+            grid: {
+                horizontalLines: false,
+                verticalLines: false
+            },
+            mouse: {
+                track: true,
+                relative: true
+            }
         }
+    );
+}
 
-        // 棒グラフの表示
-        function drawBars(title, horizontalData, horizontalXaxisLabels) {
-            Flotr.draw(
-                $("#horizontal-bars")[0],
-                [horizontalData],
-                {
-                    title: title,
-                    bars: {
-                        show: true,
-                        horizontal: true,
-                        barWidth: 0.5,
-                        shadowSize: 0,
-                        fillOpacity: 1,
-                        lineWidth: 0
-                    },
-                    xaxis: {
-                        min: 0,
-                        tickDecimals: 0
-                    },
-                    yaxis: {
-                        ticks: horizontalXaxisLabels
-                    },
-                    grid: {
-                        horizontalLines: false,
-                        verticalLines: false
-                    },
-                    mouse: {
-                        track: true
-                    }
-                }
-            );
+// 円グラフの表示
+function drawPie(dataset) {
+    'use strict';
+
+    var pieData = [];
+
+    // データの整形
+    dataset.data.forEach(function (datum) {
+        pieData.push({label: datum.municipality, data: [[0, datum.value]]});
+    });
+
+    // グラフの描画
+    Flotr.draw(
+        $("#pie")[0],
+        pieData,
+        {
+            HtmlText: false,
+            grid: {
+                verticalLines: false,
+                horizontalLines: false
+            },
+            xaxis: {
+                showLabels: false
+            },
+            yaxis: {
+                showLabels: false
+            },
+            pie: {
+                show: true,
+                explode: 6 // 6ピクセル中心から押し出す
+            },
+            mouse: {
+                track: true,
+                relative: true
+            },
+            legend: {
+                position: 'ne', // 北東に凡例を表示
+                backgroundColor: '#D2E8FF'
+            }
         }
+    );
+}
 
-        // 円グラフの表示
-        function drawPie(pieData) {
-            Flotr.draw(
-                $("#pie")[0],
-                pieData,
-                {
-                    HtmlText: false,
-                    grid: {
-                        verticalLines: false,
-                        horizontalLines: false
-                    },
-                    xaxis: {
-                        showLabels: false
-                    },
-                    yaxis: {
-                        showLabels: false
-                    },
-                    pie: {
-                        show: true,
-                        explode: 6
-                    },
-                    mouse: {
-                        track: true
-                    },
-                    legend: {
-                        position: 'ne',
-                        backgroundColor: '#D2E8FF'
-                    }
-                }
-            );
-        }
+$(document).ready(function () {
+    'use strict';
 
-        var pathname = location.pathname;
-        var lastIndex = pathname.lastIndexOf("/");
-        var jsonPathname = pathname.substring(0, lastIndex) + ".json";
+    // 取得するJSONのパス名を求める
+    var pathname = location.pathname;
+    var lastIndex = pathname.lastIndexOf("/");
+    var jsonPathname = pathname.substring(0, lastIndex) + ".json";
 
-        $.getJSON(jsonPathname, function (dataset) {
-            formatData(dataset, horizontalData, horizontalXaxisLabels, pieData);
-
-            drawBars(dataset.dataset, horizontalData, horizontalXaxisLabels);
-            drawPie(pieData);
-            $(".flotr-dummy-div").parent().hide();
-        });
-
-    }
-);
+    $.getJSON(jsonPathname, function (dataset) {
+        // 棒グラフの描画
+        drawBars(dataset);
+        // 円グラフの描画
+        drawPie(dataset);
+        $(".flotr-dummy-div").parent().hide();
+    });
+});
